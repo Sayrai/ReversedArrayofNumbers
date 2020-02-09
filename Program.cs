5a49107ba9cd9112c00bab9950237a38503701a0
omoikesarah@gmail.com <repo@carfacts.ng>
2020-02-09 10:47:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumbersFunc
+{
+    class Program
+    {
+        private int[] listOfNumbers;
+        private static string userInputs;
+
+
+
+        //public object FirstNumber { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number Game");
+
+            Console.Write("Enter Four Number");
+            int listOfNumbers = int.Parse(Console.ReadLine());
+         
+            Console.WriteLine("Your reversed numbers are : " + userInputs);
+            Console.ReadLine();
+
+        }
+
+
+        public static int ReversedArrayOfNumbers(int[] listOfNumbers)
+        {
+            
+            int[] arrInt = listOfNumbers;
+           
+            Array.Reverse(arrInt);
+
+            return arrInt[3];
+        }
+    }
+}
